92e0071cdd41ded72ac9bf893691da8fc21556fb
Leonardo <leodg6@gmail.com>
2023-04-09 16:57:31 -0600

    Correcion

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -2,7 +2,7 @@ import "./Header.css";
 
 function Header() {
     return <header className="header">
-        return <img src='/img/header.png' alt='Org' />
+        <img src='/img/header.png' alt='Org' />
     </header>
 }
 
